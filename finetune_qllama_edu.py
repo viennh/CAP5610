d854dbd2b62737324d6b8edb2acbf7609bb5a037
Vincent Nguyen <vincent@banvien.com>
2025-11-19 11:12:19 -0500

    Remove huggingface token

--- finetune_qllama_edu.py
+++ finetune_qllama_edu.py
@@ -51,14 +51,13 @@ eval_ds = dataset["test"]
 
 # ---- Load quantized model and prepare for k-bit training ----
 bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
+    load_in_4bit=True,  # bitsandbytes 4-bit quant
     bnb_4bit_quant_type="nf4",
     bnb_4bit_use_double_quant=True,
     bnb_4bit_compute_dtype=torch.bfloat16
 )
 model = AutoModelForCausalLM.from_pretrained(
     MODEL_NAME,
-    load_in_4bit=True,                # bitsandbytes 4-bit quant
     device_map="auto",
     quantization_config=bnb_config,
     # quantization_config=bnb.nn.quantization.QuantizationConfig(
